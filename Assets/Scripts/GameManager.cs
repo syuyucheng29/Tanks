54bfd3ee0c6630d1d16a8a289581bd4b0996d808
syuyucheng29 <syuyucheng29@gmail.com>
2021-12-16 15:56:17 +0800

    Join game room by button clicked

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,25 +22,34 @@ public class GameManager : MonoBehaviourPunCallbacks
         DontDestroyOnLoad(gameObject);
         instance = this;
     }
-
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
         PhotonNetwork.GameVersion = gameVersion;
     }
-
     public override void OnConnected()
     {
         Debug.Log("PUN Connected");
     }
-
     public override void OnConnectedToMaster()
     {
         Debug.Log("PUN Connected to Master");
     }
-
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
+    }
+    public void JoinGameRoom()
+    {
+        var options = new RoomOptions
+        {
+            MaxPlayers = 6
+        };
+
+        PhotonNetwork.JoinOrCreateRoom("Kingdom", options, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room!!");
     }
 }
