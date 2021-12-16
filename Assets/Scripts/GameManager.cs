ae76b8157c3e738957daf3c823a6bcc503690f98
syuyucheng29 <syuyucheng29@gmail.com>
2021-12-16 15:51:41 +0800

    Connect to server

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+
+{
+    public static GameManager instance;
+    string gameVersion = "1";
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogErrorFormat(gameObject,
+                    "Multiple instances of {0} is not allow", GetType().Name);
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("PUN Connected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Connected to Master");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
+    }
+}
